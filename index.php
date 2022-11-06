1a228003254e7e51acdecb5c622f80d8b7460504
Alena <a.yancheuskaya@gmail.com>
2022-11-06 14:16:08 +0100

    add exceptions

--- index.php
+++ index.php
@@ -9,9 +9,28 @@ namespace App;
 // error_reporting(0);
 // ini_set('diaplay_errors', '0');
 
+require_once('./Exception/AppException.php');
+require_once('./Exception/StorageException.php');
+require_once('./Exception/ConfigurationException.php');
 require_once('./src/Controller.php');
 require_once('./src/utils/debug.php');
 require_once('./config/config.php');
+
+use App\Exception\AppException;
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
+try {
+    Controller::initConfiguration($configuration);
+    $controller = new Controller($_GET, $_POST);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystapil blad w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (Throwable $e) {
+    echo "<h1>Wystapil blad w aplikacji</h1>";
+}
 
 Controller::initConfiguration($configuration);
 $controller = new Controller($_GET, $_POST);
