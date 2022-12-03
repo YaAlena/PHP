1ba0a4114cec3837fdfc2fe9f39a6c45046d8aca
Alena <a.yancheuskaya@gmail.com>
2022-12-03 13:33:59 +0100

    add edit.php

--- templates/pages/show.php
+++ templates/pages/show.php
@@ -15,6 +15,11 @@
                     <button>Powrot do listy notatek</button>
                 </a>
             </li>
+            <li>
+                <a href="/?action=edit&id=<?php echo $note['id'] ?>">
+                    <button>Edytuj</button>
+                </a>
+            </li>
         </ul>
     <?php else : ?>
         <div>Brak notatki do wyswietlenia</div>
